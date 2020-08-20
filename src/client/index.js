bf31488b79675aabdb0e1ce2f7109eb8e9f5545e
Filippo Massarelli <filippo.massarelli@gmail.com>
2020-08-20 19:02:13 +0200

    create folder structure and requires files (empty)

---
+++ src/client/index.js
@@ -0,0 +1,2 @@
+// Here we import all styles and client side Js functions, then export
+// for webpack to take as entry point and bundle everything up
---
+++ src/server/server.js
@@ -0,0 +1,2 @@
+// Here we set up the server, define the endpoints and run
+// server side js functions that need hiding from the client e.g. APIs
