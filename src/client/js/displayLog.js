4ba5140f5cc236249820392208a85e5a9dbd8686
Filippo Massarelli <filippo.massarelli@gmail.com>
2020-08-29 12:40:54 +0200

    add functionality of displayLog to change DOM in oredr to display saved trips in UI -- next: add functionality to delete trips from log

--- src/client/js/displayLog.js
+++ src/client/js/displayLog.js
@@ -6,6 +6,28 @@
 function displayLog(data) {
   console.log("displayLog running, data received from server is");
   console.log(data);
+
+  //Clear results UI
+  const loggedResults = document.getElementById("saved-trips");
+  loggedResults.innerHTML = "";
+  loggedResults.style.display = "block";
+
+  data.map((trip) => {
+    const card = document.createElement("div");
+    card.classList.add("log-card");
+
+    const weatherImgSrc = `http://openweathermap.org/img/wn/${trip.icon}@2x.png`;
+    const resultsHTML = `
+                        <img src="${trip.cityImgSrc}">
+                        <div class="card">
+                        <h1>${trip.city}</h1>
+                        <img src="${weatherImgSrc}">
+                        <p>${trip.temp} °C with ${trip.description}</p>
+                        <button id="delete">Delete Trip</button>
+                        </div>`;
+    card.insertAdjacentHTML("beforeend", resultsHTML);
+    loggedResults.appendChild(card);
+  });
 }
 
 export { displayLog };
